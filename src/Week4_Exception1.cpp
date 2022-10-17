a6e3691fdc5aac3fbd46486d01c83e68f7738432
Grec88 <91214571+Grec88@users.noreply.github.com>
2022-10-17 09:14:59 +0300

    The project is added

---
+++ src/Week4_Exception1.cpp
@@ -0,0 +1,30 @@
+//============================================================================
+// Name        : Week4_Exception1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <exception>
+#include <sstream>
+using namespace std;
+
+void EnsureEqual(const string& left, const string& right){
+	if(left != right){
+		stringstream ss;
+		ss << left << " != " << right;
+		throw runtime_error(ss.str());
+	}
+}
+
+int main() {
+  try {
+    EnsureEqual("C++ White", "C++ White");
+    EnsureEqual("C++ White", "C++ Yellow");
+  } catch (runtime_error& e) {
+    cout << e.what() << endl;
+  }
+  return 0;
+}
